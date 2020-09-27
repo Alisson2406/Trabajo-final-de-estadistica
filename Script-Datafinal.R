fe81549ad532352eed0c24d28ad46815ffabdd10
Alisson2406 <70908581+Alisson2406@users.noreply.github.com>
2020-09-27 09:57:49 -0500

    Add files via upload

---
+++ Script-Datafinal.R
@@ -0,0 +1,24 @@
+
+#MergeFinalData
+library(rio)
+#Importar
+
+LinkUrbano="https://github.com/fiorelabarbaran/Trabajo/raw/master/urbano.csv"
+Urbano=import(LinkUrbano)
+
+LinkRiqueza="https://raw.githubusercontent.com/Alisson2406/Trabajo-final-de-estadistica/master/Riqueza.csv"
+Riqueza=import(LinkRiqueza)
+
+
+LinkEducacion="https://raw.githubusercontent.com/Giancarlo-Ramirez/Estad-stica-2---Teor-a/master/EducacionFinal.csv"
+Educacion=import(LinkEducacion)
+
+
+LinkDemocracia="https://raw.githubusercontent.com/AlonsoAbarca/TrabajoFinal_Estadistica2/master/Democraciafinal.csv"
+Democracia=import(LinkDemocracia)
+
+Datafinal=merge(Democracia,Educacion)
+Datafinal=merge(Datafinal,Riqueza)
+Datafinal=merge(Datafinal,Urbano)
+write.csv(Datafinal,"Datafinal.csv", row.names = F)
+
